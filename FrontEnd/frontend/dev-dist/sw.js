f04309ecd36d82148ba4c06ff062e5eb8bef33be
kevingfa23 <ia.kevinflores23@ufg.edu.sv>
2025-11-05 00:28:31 -0600

    .

--- FrontEnd/frontend/dev-dist/sw.js
+++ FrontEnd/frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-041f2bd8'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.g9hs2liaerg"
+    "revision": "0.o7i1qk08n18"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
